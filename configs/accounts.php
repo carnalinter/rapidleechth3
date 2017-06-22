00ecde6a113b801913aa449db5b23fb7bcaa40fa
carnalinter <carnalintercourse11@gmail.com>
2017-06-22 10:49:23 +0630

    Update accounts.php

--- configs/accounts.php
+++ configs/accounts.php
@@ -8,15 +8,7 @@ $premium_acc = array();
 //$premium_acc["rapidshare_com"] = array(array('user' => 'your username1', 'pass' => 'your password1'),array('user' => 'your username2', 'pass' => 'your password2'),array('user' => 'your username3', 'pass' => 'your password3'));
 
 
-//$premium_acc['dl.sammobile.com'] = array('user' => 'susutun', 'pass' => '095112233sam');
-$premium_acc["userscloud_com"] = array('user' => 'susutun', 'pass' => '394277@usercloud!@#$%');
-$premium_acc["sammobile_com"] = array('user' => 'susutun', 'pass' => '095112233sam123');
-$premium_acc['uptobox_com'] = array('user' => 'kokar', 'pass' => 'kokar123');
-$premium_acc["my_pcloud_com"] = array('user' => 'eieikhaing.cn@gmail.com', 'pass' => 'K@nainG2050');
-$premium_acc["rapidgator_net"] = array('user' => 'aungminhan123@gmail.com', 'pass' => 'aungminhan1993');
-$premium_acc["samsung-firmware_org"] = array('user' => 'susutun', 'pass' => 'samsung-firmware.org');
-$premium_acc["4shared_com"] = array('user' => 'susutunum2@gmail.com', 'pass' => '394277');
-//$premium_acc["openload_co"] = array('user' => 'susutunum2@gmail.com', 'pass' => 'openload.iosusutun');
+
 //$premium_acc["netload_in"] = array('user' => 'your username', 'pass' => 'your password');
 //$premium_acc["cramit_in"] = array('user' => 'your username', 'pass' => 'your password');
 //$premium_acc["megashare_com"] = array('user' => 'your username', 'pass' => 'your password');
